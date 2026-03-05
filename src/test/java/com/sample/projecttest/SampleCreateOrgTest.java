691bdabe823bb994edfa3bb99d49f94260c70a32
apoorvasai898 <testingas989@gmail.com>
2026-03-05 11:21:03 +0530

    changes in group

--- src/test/java/com/sample/projecttest/SampleCreateOrgTest.java
+++ src/test/java/com/sample/projecttest/SampleCreateOrgTest.java
@@ -3,13 +3,13 @@ package com.sample.projecttest;
 import org.testng.annotations.Test;
 		
 public class SampleCreateOrgTest {
-	@Test(groups = "ST")
+	@Test(groups = "RT")
 	public void createOrg1Test()
 	{
 		System.out.println("Organization 1");
 	}
 	
-	@Test
+	@Test(groups = "RT")
 	public void createOrg2Test()
 	{
 		System.out.println("Organization 2");
